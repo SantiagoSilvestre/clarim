46ac43e0c708b4de127e6075989c9d8fbfcd0dda
Santiago <silvestre.santiagosilvestre732@gmail.com>
2020-07-29 18:21:59 -0300

    melhorando mata-mata

--- public/assets/js/jogo_mata.js
+++ public/assets/js/jogo_mata.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 
     idc = $("#id_cam").val();
     contador = $("#contador").val();
+    fase = $("#fase").val();
     contador2 = contador;
     $.ajax({
         type: 'GET',
@@ -11,21 +12,28 @@ $(document).ready(function () {
             window.location.href = "/adm/campeonatos";
         },
         success: function success(data) {
-           while(contador > 0) {
-                data.forEach(element => {
-                    $("#time1_"+contador+"").append('<option value="'+element.id+'">'+element.time+'</option>');
-                    $("#time2_"+contador+"").append('<option value="'+element.id+'">'+element.time+'</option>'); 
-                });
-                contador --;
-           } 
+            console.log(data);
+            if(data) {
+
+                while(contador > 0) {
+                    data.forEach(element => {
+                        $("#time1_"+contador+"").append('<option value="'+element.id+'">'+element.time+'</option>');
+                        $("#time2_"+contador+"").append('<option value="'+element.id+'">'+element.time+'</option>'); 
+                    });
+                    contador --;
+               } 
+            } else {
+                window.location.href = "/adm/campeonatos";
+            }
         },
         dataType: 'json'
     });
 
     
     $("#SendCadJog").on('click', function(){
-        var times = montaArray();
-        times.push(idc);
+        var times = montaArray(idc, fase);
+        //times.push(idc);
+        console.log(times);
         $.ajax({
             type: 'POST',
             url: '/adm/proc_cad_mata',
@@ -38,6 +46,8 @@ $(document).ready(function () {
                 console.log(data);
                if(data == 0) {
                 window.location.href = "/adm/campaonato/jogo?id="+idc;
+               } else if(data == 1) {
+                window.location.href = "/adm/campeonatos";
                }
             },
             dataType: 'json'
@@ -47,13 +57,13 @@ $(document).ready(function () {
        
 });
 
-function montaArray() {
+function montaArray(idc, fase) {
     var times = [];
     while(contador2 > 0 ) {
         var meuObj = new Object();
         var idt1 = $("#time1_"+contador2+"").val();
         var idt2 = $("#time2_"+contador2+"").val();
-        meuObj =  [ idt1, idt2];
+        meuObj =  [ idt1, idt2, idc, fase];
         times.push(meuObj);
         contador2 --;
     }
