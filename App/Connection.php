348205ae5e43cdfdfeba1cf45b2043952102d0f8
Santiago Silvestre <49048537+SantiagoSilvestre@users.noreply.github.com>
2023-10-16 11:24:35 -0300

    Update Connection.php

--- App/Connection.php
+++ App/Connection.php
@@ -9,7 +9,7 @@
             try
             {
                 $conn = new \PDO("mysql:host=localhost;dbname=clarim;charset=utf8",
-                "root@177.62.96.82", "s0202903526@");
+                "user", "password");
 
                 return $conn;
 
